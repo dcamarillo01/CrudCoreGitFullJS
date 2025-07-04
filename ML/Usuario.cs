0bf3368748f6f90b476382a9ab1e64418e50bebe
dcamarillo01 <dcamarillo01@hotmail.com>
2025-07-04 13:30:24 -0600

    Agregue propiedadades a ML.Result y ML.Usuario, tambien agregue metodos Update y GetAll en BL

--- ML/Usuario.cs
+++ ML/Usuario.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ML
 {
-    internal class Usuario
+    public class Usuario
     {
+
+        public int IdUsuario { get; set; }
+        public string? Nombre { get; set; }
+        public string? ApellidoPaterno { get; set; }
+        public DateOnly? FechaNacimiento { get; set; }
+        public List<object> Usuarios { get; set; }
     }
 }
