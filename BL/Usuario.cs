0640b11ca93237319f90e14648d866ddf3c3efbc
dcamarillo01 <dcamarillo01@hotmail.com>
2025-07-04 14:31:06 -0600

    Arreglo de estructura BL

--- BL/Usuario.cs
+++ BL/Usuario.cs
@@ -18,6 +18,69 @@ namespace BL
         
             _context = context;
         }
+        //Metodo mostrar usuario por id con LinQ
+        public ML.Result GetById(int idUsuario)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                var query = _context.Usuarios.FirstOrDefault(usuario => usuario.IdUsuario == idUsuario);
+                if (query != null)
+                {
+                    result.Correct = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+        //Metodo agregar con Linq
+        public ML.Result Add(DL.Usuario usuario)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                var query = _context.Usuarios.Add(usuario);
+                if (query != null)
+                {
+                    _context.SaveChanges();
+                    result.Correct = true;
+                }
+            }
+            catch (Exception ex) 
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+        //Metodo eliminar con LinQ
+        public ML.Result Delete(int idUsuario)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                var query = _context.Usuarios.FirstOrDefault(usuario => usuario.IdUsuario == idUsuario);
+                if (query != null)
+                {
+                    _context.Usuarios.Remove(query);
+                    _context.SaveChanges();
+                    result.Correct = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
 
 
         public ML.Result Update(int IdUsuario, ML.Usuario Usuario) { 
@@ -109,70 +172,6 @@ namespace BL
 
             return result;
 
-        }
-        //Metodo mostrar usuario por id con LinQ
-        public ML.Result GetById(int idUsuario)
-        {
-            ML.Result result = new ML.Result();
-            try
-            {
-                var query = _context.Usuarios.FirstOrDefault(usuario => usuario.IdUsuario == idUsuario);
-                if (query != null)
-                {
-                    result.Correct = true;
-                }
-            }
-            catch (Exception ex)
-            {
-                result.Correct = false;
-                result.ErrorMessage = ex.Message;
-                result.Ex = ex;
-            }
-            return result;
-        }
-        //Metodo agregar con Linq
-        public ML.Result Add(DL.Usuario usuario)
-        {
-            ML.Result result = new ML.Result();
-            try
-            {
-                var query = _context.Usuarios.Add(usuario);
-                if (query != null)
-                {
-                    _context.SaveChanges();
-                    result.Correct = true;
-                }
-            }
-            catch (Exception ex) 
-            {
-                result.Correct = false;
-                result.ErrorMessage = ex.Message;
-                result.Ex = ex;
-            }
-            return result;
-        }
-        //Metodo eliminar con LinQ
-        public ML.Result Delete(int idUsuario)
-        {
-            ML.Result result = new ML.Result();
-            try
-            {
-                var query = _context.Usuarios.FirstOrDefault(usuario => usuario.IdUsuario == idUsuario);
-                if (query != null)
-                {
-                    _context.Usuarios.Remove(query);
-                    _context.SaveChanges();
-                    result.Correct = true;
-                }
-            }
-            catch (Exception ex) 
-            {
-                result.Correct = false;
-                result.ErrorMessage = ex.Message;
-                result.Ex = ex;
-            }
-
-            return result;
         }
     }
 }
