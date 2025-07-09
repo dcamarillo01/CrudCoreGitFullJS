c6884f880bbc6fd1bca9ec96e3d657efa04ef4f6
Jenifergg25 <jenifergg1@outlook.com>
2025-07-09 10:06:25 -0600

    Hola buen dia compañero, ya quedo lo de update :D

--- DL/Usuario.cs
+++ DL/Usuario.cs
@@ -5,10 +5,10 @@ namespace DL;
 
 public partial class Usuario
 {
-    public int IdUsuario { get; set; }
+    public int IdUsuario { get; set; } 
 
     public string? Nombre { get; set; } 
-
+     
     public string? ApellidoPaterno { get; set; }
 
     public DateOnly? FechaNacimiento { get; set; }
